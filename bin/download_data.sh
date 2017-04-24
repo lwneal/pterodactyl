073c6b36fefc03cd4486f3e94a4e3c3fc15d942d
Larry Neal <nealla@lwneal.com>
2017-04-23 17:06:42 -0700

    Add script to download example labeled data

---
+++ bin/download_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -nc lwneal.com/hja_sarah_664.tar.gz
+tar xzvf hja_sarah_664.tar.gz
+
+wget -nc lwneal.com/hja_lawrence_625.tar.gz
+mkdir hja_lawrence_625
+pushd hja_lawrence_625
+tar xzvf hja_lawrence_625.tar.gz
+popd
+
